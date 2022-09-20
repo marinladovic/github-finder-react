9cdbb5fdaca98fcf30772f35cc58a9d9735f5100
marin <mladovic.dev@gmail.com>
2022-09-20 16:49:20 +0200

    fetch users and display on home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import UserResults from '../components/users/UserResults';
+
+function Home() {
+  return (
+    <>
+      {/* @todo: Search component goes here */}
+      <UserResults />
+    </>
+  );
+}
+
+export default Home;
